ec2f5b1d5d2da902a4cc8ec91e810b2f1f970642
BannaHinder <hanna.b.ekedahl@outlook.com>
2022-04-17 02:02:50 +0200

    can finally upload book to strapi WITH relationsgit add .

--- frontend/main.js
+++ frontend/main.js
@@ -1,202 +1,255 @@
 //Inlogg strapi:
-//Name: Hanna   
+//Name: Hanna
 //Email: hanna@email.com
 //Password: Hanna1234
 
 //User-1:
 //Name: User1
-//Email: User1@email.com 
+//Email: User1@email.com
 //Password: User1111
 
 //etc
 
 //Toggle log-in
-let userLoginPage = document.querySelector(".login-user")
-let userRegisterPage = document.querySelector(".register-user")
-document.querySelector("#toggle-login-btn").addEventListener("click", ()=>{
-    document.querySelector(".login-user").classList.toggle('hide')
-    //document.querySelector('.register-user').classList.toggle('hide')
-
-})
-document.querySelector('#toggle-register-btn').addEventListener('click', ()=>{
-    document.querySelector('.register-user').classList.toggle('hide')
-    document.querySelector(".login-user").classList.toggle('hide')
-})
+let userLoginPage = document.querySelector(".login-user");
+let userRegisterPage = document.querySelector(".register-user");
+document.querySelector("#toggle-login-btn").addEventListener("click", () => {
+  document.querySelector(".login-user").classList.toggle("hide");
+  //document.querySelector('.register-user').classList.toggle('hide')
+});
+document.querySelector("#toggle-register-btn").addEventListener("click", () => {
+  document.querySelector(".register-user").classList.toggle("hide");
+  document.querySelector(".login-user").classList.toggle("hide");
+});
 
 //Log-in
-document.querySelector('#login-btn').addEventListener('click', async ()=>{
-    const userName = document.querySelector('#username-login')
-    const password = document.querySelector('#password-login')
-    let response = await axios.post("http://localhost:1337/api/auth/local", {
-        identifier: userName.value,
-        password: password.value
-    });
-    console.log(response);
-    sessionStorage.setItem("token", response.data.jwt);
-    checkLoginStatus()
-})
+document.querySelector("#login-btn").addEventListener("click", async () => {
+  const userName = document.querySelector("#username-login");
+  const password = document.querySelector("#password-login");
+  let response = await axios.post("http://localhost:1337/api/auth/local", {
+    identifier: userName.value,
+    password: password.value,
+  });
+  console.log(response);
+  sessionStorage.setItem("token", response.data.jwt);
+  checkLoginStatus();
+});
 
 //register new user
-document.querySelector("#register-btn").addEventListener("click", async ()=>{
-    let username = document.querySelector('#username-register').value
-    let email = document.querySelector('#email-register').value
-    let password = document.querySelector('#password-register').value
-    try {
-        let response = await axios.post(
-            "http://localhost:1337/api/auth/local/register",
-        {
-            username,
-            password,
-            email,
-        });
-        console.log(response);
-        sessionStorage.setItem("token", response.data.jwt);
-        username = email = password = '';
-    } catch (error) {
-        console.log(error)
-        username = email = password = '';
-    }
-    
-})
+document.querySelector("#register-btn").addEventListener("click", async () => {
+  let username = document.querySelector("#username-register").value;
+  let email = document.querySelector("#email-register").value;
+  let password = document.querySelector("#password-register").value;
+  try {
+    let response = await axios.post(
+      "http://localhost:1337/api/auth/local/register",
+      {
+        username,
+        password,
+        email,
+      }
+    );
+    console.log(response);
+    sessionStorage.setItem("token", response.data.jwt);
+    username = email = password = "";
+  } catch (error) {
+    console.log(error);
+    username = email = password = "";
+  }
+});
 
 //CHeck if logged in func
 const checkLoginStatus = () => {
-    if (sessionStorage.getItem("token")){
-        document.querySelector(".login-panel").classList.add("hide");
-        document.querySelector(".header-icons").classList.remove("hide");
-    }
-}
+  if (sessionStorage.getItem("token")) {
+    document.querySelector(".login-panel").classList.add("hide");
+    document.querySelector(".header-icons").classList.remove("hide");
+  }
+};
 //log out user
-const logOut = ()=>{
-sessionStorage.removeItem("token")
-document.querySelector(".login-panel").classList.remove("hide");
-document.querySelector(".header-icons").classList.add("hide");
-document.querySelector(".login-user").classList.add("hide")
-document.querySelector(".user-profile-popup").classList.add("hide")
-document.querySelector(".add-book").classList.add("hide")
-}
-
+const logOut = () => {
+  sessionStorage.removeItem("token");
+  document.querySelector(".login-panel").classList.remove("hide");
+  document.querySelector(".header-icons").classList.add("hide");
+  document.querySelector(".login-user").classList.add("hide");
+  document.querySelector(".user-profile-popup").classList.add("hide");
+  document.querySelector(".add-book").classList.add("hide");
+};
 
 // fetch data
-const getProducts = async (product) =>{
- let {data:{data}} = await axios.get(`http://localhost:1337/api/${product}?populate=*`)
- console.log(data)
- return data;
-}
+const getProducts = async (product) => {
+  let {
+    data: { data },
+  } = await axios.get(`http://localhost:1337/api/${product}?populate=*`);
+  console.log(data);
+  return data;
+};
 //Render Books
-const renderBooks = async ()=>{
-    let bookList = document.querySelector(".book-list");
-    bookList.innerHTML='';
-    let books = await getProducts("books");
-    books.forEach(book => {
-        let genreString = ''
-        book.attributes.genres.data.forEach((item) =>{
-            genreString+= item.attributes.genre + ', '
-        })
+const renderBooks = async () => {
+  let bookList = document.querySelector(".book-list");
+  bookList.innerHTML = "";
+  let books = await getProducts("books");
+  books.forEach((book) => {
+    let genreString = "";
+    book.attributes.genres.data.forEach((item) => {
+      genreString += item.attributes.genre + ", ";
+    });
 
-        const li = document.createElement('li')
-        li.innerHTML= `<h4>${book.attributes.title}</h4>
+    const li = document.createElement("li");
+    li.innerHTML = `<h4>${book.attributes.title}</h4>
         <p>Author: ${book.attributes.author}, page count: ${book.attributes.pages}</p>
         <p> Genre(s): ${genreString} Rating: ${book.attributes.rating}</p>
         <img src="http://localhost:1337${book.attributes.cover.data.attributes.url}" height="150" width="100" alt="cover-art">
-        <p>Lender: ${book.attributes.user.data.attributes.username}, Email: ${book.attributes.user.data.attributes.email}</p>`
-        bookList.append(li)
-    });
-    //return bookList
-}
+        <p>Lender: ${book.attributes.user.data.attributes.username}, Email: ${book.attributes.user.data.attributes.email}</p>`;
+    bookList.append(li);
+  });
+  //return bookList
+};
 //render Audiobooks
-const renderAudioBooks = async ()=>{
-    let bookList = document.querySelector(".audio-book-list");
-    bookList.innerHTML='';
-    let books = await getProducts("audio-books");
-    books.forEach(book => {
-        const li = document.createElement('li')
-        let genreString = ''
-        book.attributes.genres.data.forEach((item) =>{
-            genreString+= item.attributes.genre  + ', '
-        })
-        li.innerHTML= `<h4>${book.attributes.title}</h4>
+const renderAudioBooks = async () => {
+  let bookList = document.querySelector(".audio-book-list");
+  bookList.innerHTML = "";
+  let books = await getProducts("audio-books");
+  books.forEach((book) => {
+    const li = document.createElement("li");
+    let genreString = "";
+    book.attributes.genres.data.forEach((item) => {
+      genreString += item.attributes.genre + ", ";
+    });
+    li.innerHTML = `<h4>${book.attributes.title}</h4>
         <p>Release date: ${book.attributes.release_date}, duration: ${book.attributes.minutes} min</p>
         <p> Genre(s): ${genreString} Rating: ${book.attributes.rating}</p>
         <img src="http://localhost:1337${book.attributes.cover.data.attributes.url}" height="150" width="100" alt="cover-art">
-        <p>Lender: ${book.attributes.user.data.attributes.username}, Email: ${book.attributes.user.data.attributes.email}</p></p>`
-        bookList.append(li)
-    });
-    //return bookList
-}
+        <p>Lender: ${book.attributes.user.data.attributes.username}, Email: ${book.attributes.user.data.attributes.email}</p></p>`;
+    bookList.append(li);
+  });
+  //return bookList
+};
 //toggle user profile
-document.querySelector(".fa-user").addEventListener("click", ()=>{
-    document.querySelector(".user-profile-popup").classList.toggle("hide")
-    renderProfile()
-})
-//användarnamn, email, id, samt vilket datum användaren registrerade sig. 
-//(VG) Sidan ska även innehålla en lista över samtliga böcker och ljudböcker
-// som användaren har lagt ut för utlåning.
+document.querySelector(".fa-user").addEventListener("click", () => {
+  document.querySelector(".user-profile-popup").classList.toggle("hide");
+  renderProfile();
+});
 
-//render user profile ,maybe the user Array should be done simultaneously 
+//render user profile ,maybe the user Array should be done simultaneously
 //when logging in so there's not too many fetches when opening user profile??
 // if (userArr= []){fetch data}
 
-const renderProfile = async ()=>{
- const user = await getUserData()
- const books = await getProducts("books")
- const audioBooks = await getProducts("audio-books")
- let userBooksArr = [];
- books.forEach(book =>{
-     if(book.attributes.user.data.id ==user.id){
-         userBooksArr.push(book)}
- })
- audioBooks.forEach(book =>{
-    if(book.attributes.user.data.id ==user.id){
-        userBooksArr.push(book)}
-})
-console.log(userBooksArr)
-let container = document.querySelector(".profile-details")
-container.innerHTML=`<h3>${user.username}</h3>
+const renderProfile = async () => {
+  const user = await getUserData();
+  const books = await getProducts("books");
+  const audioBooks = await getProducts("audio-books");
+  let userBooksArr = [];
+
+  books.forEach((book) => {
+    if (book.attributes.user.data.id == user.id) {
+      userBooksArr.push(book);
+    }
+  });
+  audioBooks.forEach((book) => {
+    if (book.attributes.user.data.id == user.id) {
+      userBooksArr.push(book);
+    }
+  });
+  console.log(userBooksArr);
+  let container = document.querySelector(".profile-details");
+  container.innerHTML = `<h3>${user.username}</h3>
 <p>Email: ${user.email}</p>
 <p>Id: ${user.id}</p>
-<p>Registered: ${user.createdAt}</p>`
-//skriv ut arrrayen
-
-}
-
-
+<p>Registered: ${user.createdAt}</p>`;
+  //skriv ut arrrayen
+};
 
 //toggle add book/audiobook
-const addBookIcon = document.querySelector(".fa-plus")
-const addBook = document.querySelector(".add-book")
-const selectBook = document.querySelector("#book-type")
-const bookInput = document.querySelector(".book-input")
-const audioInput = document.querySelector(".audio-input")
-addBookIcon.addEventListener("click", ()=>{
-    addBook.classList.toggle("hide")
-})
-//toggle input
-selectBook.addEventListener("change", (x)=>{
-    if(x.target.value=="book"){
-        bookInput.classList.remove("hide")
-        audioInput.classList.add("hide")
-    }else if(x.target.value=="audio"){
-        audioInput.classList.remove("hide")
-        bookInput.classList.add("hide")
-    }
-})
+const addBookIcon = document.querySelector(".plus-sign");
+const addBook = document.querySelector(".add-book");
+const selectBook = document.querySelector("#book-type");
+const bookInput = document.querySelector(".book-input");
+const audioInput = document.querySelector(".audio-input");
+const newBookInput = document.querySelector(".new-book-input");
 
+addBookIcon.addEventListener("click", () => {
+  addBook.classList.toggle("hide");
+});
+
+//toggle input
+selectBook.addEventListener("change", (x) => {
+  if (x.target.value == "book") {
+    newBookInput.classList.remove("hide");
+    bookInput.classList.remove("hide");
+    audioInput.classList.add("hide");
+  } else if (x.target.value == "audio-book") {
+    newBookInput.classList.remove("hide");
+    audioInput.classList.remove("hide");
+    bookInput.classList.add("hide");
+  }
+});
 
 //Lägga till bok (post) till en users lista
+const getBook = async () => {
+  const { id } = await getUserData();
+  let genres = [];
+  $('input[name="genre"]:checked').each(function () {
+    genres.push(parseInt(this.value));
+  });
+  let image = document.querySelector("#book-cover").files;
+  let imgData = new FormData();
+  imgData.append("files", image[0]);
 
+  const type = document.querySelector("#book-type").value;
+  const title = document.querySelector("#book-title").value;
+  const author = document.querySelector("#book-author").value;
+  const pages = document.querySelector("#book-page").value;
+  const rating = document.querySelector("#book-rating").value;
+  const release_date = document.querySelector("#audio-date").value;
+  const minutes = document.querySelector("#audio-min").value;
 
+  console.log([parseInt(id)])
+  console.log(genres)
+  axios
+    .post("http://localhost:1337/api/upload", imgData, {
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+      },
+    })
+    .then((res) => {
+      let imageId = res.data[0].id;
+      axios.post(
+        `http://localhost:1337/api/${type}s`,
+        {
+          data: {
+            title,
+            author,
+            pages,
+            rating,
+            release_date,
+            minutes,
+            genres,
+            user: [parseInt(id)],
+            cover: imageId,
+          },
+        },
+        {
+          headers: {Authorization: `Bearer ${sessionStorage.getItem("token")}`,},
+        }
+      );
+    })
+    .catch((error) => {
+      console.log("something went wrong with file upload");
+      console.log(error);
+    });
+};
 
 //check current user data
 const getUserData = async () => {
-    let {data} = await axios.get("http://localhost:1337/api/users/me",
-    {
-        headers: {
-            Authorization: `Bearer ${sessionStorage.getItem("token")}`
-        }
-    })
-    console.log(data);
-    return data
-}
+  let { data } = await axios.get("http://localhost:1337/api/users/me", {
+    headers: {
+      Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+    },
+  });
+  console.log(data);
+  return data;
+};
 
-checkLoginStatus()
+checkLoginStatus();
+window.onbeforeunload = function() {
+    return "Dude, are you sure you want to leave? Think of the kittens!";}
